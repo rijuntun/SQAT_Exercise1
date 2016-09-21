3165a1630430f5046b7716c4e0370ca4fb03e6c2
somename <someemail>
2016-09-21 16:00:10 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -33,7 +33,24 @@ public class TestBowling {
 		bg.addFrame(fr2);
 		Frame fr3=new Frame(6,4);
 		bg.addFrame(fr3);
-		assertEquals(14, bg.score());
+		Frame fr4=new Frame(5,5);
+		bg.addFrame(fr4);
+		Frame fr5=new Frame(10,0);
+		bg.addFrame(fr5);
+		Frame fr6=new Frame(0,1);
+		bg.addFrame(fr6);
+		Frame fr7=new Frame(7,3);
+		bg.addFrame(fr7);
+		Frame fr8=new Frame(6,4);
+		bg.addFrame(fr8);
+		Frame fr9=new Frame(10,0);
+		bg.addFrame(fr9);
+		Frame fr10=new Frame(2,8);
+		bg.addFrame(fr10);
+		Frame fr11=new Frame(6,0);
+		bg.addFrame(fr11);
+  
+	    assertEquals(133, bg.score());
 		
 	}
 
