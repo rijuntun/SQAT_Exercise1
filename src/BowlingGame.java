bbcd479e09879d0928ae979a6927433a64562495
somename <someemail>
2016-09-21 15:15:17 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -14,6 +14,7 @@ public class BowlingGame {
 	// adds a frame to the game
 	public void addFrame(Frame frame){
 		//to be implemented
+		frames.add(frame);
 	}
 	
 	// Sets the bonus throws at the end of the game
@@ -23,7 +24,12 @@ public class BowlingGame {
 	
 	// Returns the game score
 	public int score(){
-		//to be implemented: should return game score 
-		return 0;
+		//to be implemented: should return game score
+		int score=0;
+		for(Frame fr: frames)
+		{
+			score+=fr.score();
+		}
+		return score;
 	}
 }
