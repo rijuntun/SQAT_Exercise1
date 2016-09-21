cfe95614d8129ba9739ff5378f8957528e12710d
somename <someemail>
2016-09-21 15:07:59 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -3,6 +3,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestBowling {
+	
+	@Test
+	public void testAddFrame(){
+		BowlingGame bg= new BowlingGame();
+		Frame fr= new Frame(1,4);
+		bg.addFrame(fr);
+		assertEquals(bg.score(),5);
+		
+	}
 
 	@Test
 	public void test() {
